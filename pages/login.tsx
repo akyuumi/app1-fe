5b9ae2fee74a0bbf3564fa8568b76dd4a8d8e689
akyuumi <akyuumi.pg@gmail.com>
2023-10-29 16:46:49 +0900

    init

---
+++ pages/login.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import { NextPage } from 'next';
+
+const Login: NextPage = () => {
+  const { data: session } = useSession();
+
+  return (
+    <>
+      {
+        // セッションがある場合、ログアウトを表示
+        session && (
+          <div>
+            <h1>ようこそ, {session.user && session.user.email}</h1>
+            <button onClick={() => signOut()}>ログアウト</button>
+          </div>
+        )
+      }
+      {
+        // セッションがない場合、ログインを表示
+        // ログインボタンを押すと、ログインページに遷移する
+        !session && (
+          <div>
+            <p>ログインしていません</p>
+            <button onClick={() => signIn()}>ログイン</button>
+          </div>
+        )
+      }
+    </>
+  );
+};
+
+export default Login;
